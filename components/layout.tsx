803eb7facd586effba4c894205cc23049257d8fb
rramanouski <rramanouski@eisgroup.com>
2022-01-20 19:49:03 +0100

    Added Google Analytics.

--- components/layout.tsx
+++ components/layout.tsx
@@ -57,15 +57,14 @@ const ProtoLogo = () => (
 
 const GoogleTags = () => {
     return (
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TGHLFG4');`
-            }}
-        />
+        <>
+            <script async src='https://www.googletagmanager.com/gtag/js?id=G-BFQYJV20GB' />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-BFQYJV20GB');`
+                }}
+            />
+        </>
     )
 }
 
@@ -113,7 +112,9 @@ export default function Layout({children, home}) {
                     </>
                 )}
             </header>
-            <menu></menu>
+            <menu>
+                <div className=''></div>
+            </menu>
             <main>{children}</main>
             {!home && (
                 <div className={styles.backToHome}>
