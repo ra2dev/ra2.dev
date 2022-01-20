386ccfafd40a5a7059c79d405b0e2bbfb8fa1d21
rramanouski <rramanouski@eisgroup.com>
2022-01-20 17:27:43 +0100

    Added typescript.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import {getSortedPostsData} from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
-
 export default function Home({allPostsData}) {
     return (
         <Layout home>
@@ -13,7 +12,7 @@ export default function Home({allPostsData}) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Lorem ipsum dolor sit amet</p>
+                <a href="https://status.ra2.dev/">Status Page </a>
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
