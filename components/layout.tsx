b822238ff4868539db2af0933c7d64f8a42d73ff
rramanouski <rramanouski@eisgroup.com>
2022-01-20 18:18:49 +0100

    Added GoogleTagManager.

--- components/layout.tsx
+++ components/layout.tsx
@@ -55,6 +55,20 @@ const ProtoLogo = () => (
     </svg>
 )
 
+const GoogleTags = () => {
+    return (
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TGHLFG4');`
+            }}
+        />
+    )
+}
+
 export default function Layout({children, home}) {
     return (
         <div className={styles.container}>
@@ -69,6 +83,7 @@ export default function Layout({children, home}) {
                 />
                 <meta name='og:title' content={siteTitle} />
                 <meta name='twitter:card' content='summary_large_image' />
+                <GoogleTags />
             </Head>
             <header className={styles.header}>
                 {home ? (
