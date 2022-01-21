de88fd3f323c85e4856d6cd4016abd5707b43c1e
rramanouski <rramanouski@eisgroup.com>
2022-01-21 02:37:45 +0100

    Added layout working on spotify.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import cn from 'classnames'
+import {useEffect, useState} from 'react'
+import NextLink from 'next/link'
+import {useRouter} from 'next/router'
+import MobileMenu from './MobileMenu'
+import Footer from './Footer'
 
 const name = 'Placeholder Site v0.0.1-beta.1 🚧'
 export const siteTitle = 'Next.js Sample Website'
@@ -67,10 +69,128 @@ const GoogleTags = () => {
         </>
     )
 }
+const useTheme = (mounted?: boolean) => {
+    const isDarkThem = mounted && document?.documentElement?.className?.includes('dark')
+    const toggleTheme = () => {
+        document.documentElement.classList.toggle('dark')
+        document.documentElement.classList.toggle('light')
+    }
+    return {isDarkThem, toggleTheme}
+}
 
-export default function Layout({children, home}) {
+function NavItem({href, text}) {
+    const router = useRouter()
+    const isActive = router.asPath === href
+
     return (
-        <div className={styles.container}>
+        <NextLink href={href}>
+            <a
+                className={cn(
+                    isActive
+                        ? 'font-semibold text-gray-800 dark:text-gray-200'
+                        : 'font-normal text-gray-600 dark:text-gray-400',
+                    'hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all'
+                )}
+            >
+                <span className='capsize'>{text}</span>
+            </a>
+        </NextLink>
+    )
+}
+
+const ThemeBtn = () => {
+    const [mounted, setMounted] = useState(false)
+    const {isDarkThem, toggleTheme} = useTheme()
+
+    useEffect(() => setMounted(true), [])
+    return (
+        <button
+            aria-label='Toggle Dark Mode'
+            type='button'
+            className='w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all'
+            onClick={toggleTheme}
+        >
+            {mounted && (
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    className='w-5 h-5 text-gray-800 dark:text-gray-200'
+                >
+                    {isDarkThem ? (
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+                        />
+                    ) : (
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+                        />
+                    )}
+                </svg>
+            )}
+        </button>
+    )
+}
+
+const Logo = ({className}) => {
+    return (
+        <div
+            className={cn('text-white p-1 pr-8 bg-gray-900 dark:text-gray-800 dark:bg-gray-300 rounded-sm', className)}
+        >
+            <svg viewBox='173.747 85.849 60.823 67.68' className='h-10'>
+                <path
+                    stroke='null'
+                    d='M 214.015 97.13 L 227.843 97.13 L 227.843 305.135 L 201.162 305.135 L 201.162 97.13 L 214.015 97.13 Z'
+                    fill='currentColor'
+                    id='svg_5'
+                    transform='matrix(0.332723, 0, 0, 0.324777, 158.760773, 54.381058)'
+                />
+                <rect
+                    stroke='null'
+                    id='svg_1'
+                    height='19.864'
+                    width='48.635'
+                    y='249.364'
+                    x='110.949'
+                    fill='currentColor'
+                    transform='matrix(0.332723, 0, 0, 0.324777, 158.760773, 54.381058)'
+                />
+                <path
+                    stroke='null'
+                    d='m103.28829,304.93573l-26.73065,0l0,-82.31063l-4.7873,0l0,82.31063l-26.73065,0l0,-208.00519l45.0826,0q5.58456,0 9.37528,2.82293t3.79073,6.98864l0,82.30502a13.39541,9.98537 0 0 1 -3.79073,7.12039a12.41012,9.2509 0 0 1 -9.37528,2.9715l-18.35194,0l0,3.5658l18.35194,0a12.41012,9.2509 0 0 1 9.37528,2.99393a13.39541,9.98537 0 0 1 3.79073,7.13161l0,92.10537l-0.00001,0zm-31.51795,-125.69457l4.7873,0l0,-62.40716l-4.7873,0l0,62.40716z'
+                    fill='currentColor'
+                    id='svg_6'
+                    transform='matrix(0.332723, 0, 0, 0.324777, 158.760773, 54.381058)'
+                />
+                <path
+                    stroke='null'
+                    d='m120.68368,304.89661q-5.58456,0 -9.37528,-2.82293t-3.79073,-6.98864l0,-198.19363l26.73065,0l0,188.10173l4.77226,0l0,-188.10173l26.73065,0l0,198.19363q0,4.1601 -3.79073,6.98303t-9.36024,2.82853l-31.91658,0l0,0.00001z'
+                    fill='currentColor'
+                    id='svg_8'
+                    transform='matrix(-0.332723, 0, 0, -0.324777, 249.683329, 184.871928)'
+                />
+                <path
+                    stroke='null'
+                    d='M 182.775 97.274 L 196.603 97.274 L 196.603 305.279 L 169.922 305.279 L 169.922 97.274 L 182.775 97.274 Z'
+                    fill='currentColor'
+                    id='path-1'
+                    transform='matrix(0.332723, 0, 0, 0.324777, 158.760773, 54.381058)'
+                />
+            </svg>
+        </div>
+    )
+}
+
+export default function Layout({children}) {
+    return (
+        <div className='dark:bg-gray-900 bg-white transition duration-500'>
             <Head>
                 <link rel='icon' href='/favicon.ico' />
                 <meta name='description' content='Placeholder site' />
@@ -84,45 +204,86 @@ export default function Layout({children, home}) {
                 <meta name='twitter:card' content='summary_large_image' />
                 <GoogleTags />
             </Head>
-            <header className={styles.header}>
-                {home ? (
-                    <>
-                        <ProtoLogo />
-                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
-                    </>
-                ) : (
-                    <>
-                        <Link href='/'>
-                            <a>
-                                <Image
-                                    priority
-                                    src='/images/profile.jpg'
-                                    className={utilStyles.borderCircle}
-                                    height={108}
-                                    width={108}
-                                    alt={name}
-                                />
-                            </a>
-                        </Link>
-                        <h2 className={utilStyles.headingLg}>
-                            <Link href='/'>
-                                <a className={utilStyles.colorInherit}>{name}</a>
-                            </Link>
-                        </h2>
-                    </>
-                )}
-            </header>
-            <menu>
-                <div className=''></div>
-            </menu>
-            <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href='/'>
-                        <a>← Back to home</a>
-                    </Link>
+            <div className='flex flex-col justify-center px-8'>
+                <nav className='flex items-center justify-between w-full relative max-w-4xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900  bg-opacity-60 dark:text-gray-100'>
+                    <div className='ml-[-0.60rem] visible md:hidden'>
+                        <MobileMenu />
+                    </div>
+                    <Logo className='hidden md:inline-block' />
+                    <div className='ml-[-0.60rem]'>
+                        <NavItem href='/' text='Home' />
+                        <NavItem href='/blog' text='Blog' />
+                        <NavItem href='/snippets' text='Snippets' />
+                        <NavItem href='/about' text='About' />
+                    </div>
+                    <ThemeBtn />
+                </nav>
+            </div>
+            <main className='flex flex-col justify-center px-8'>
+                <div className='max-w-2xl mx-auto pb-16'>
+                    {children}
+                    <Footer />
                 </div>
-            )}
+            </main>
         </div>
     )
 }
+//
+// export default function Layout({children, home}) {
+//     return (
+//         <div className={styles.container}>
+//             <Head>
+//                 <link rel='icon' href='/favicon.ico' />
+//                 <meta name='description' content='Placeholder site' />
+//                 <meta
+//                     property='og:image'
+//                     content={`https://og-image.vercel.app/${encodeURI(
+//                         siteTitle
+//                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+//                 />
+//                 <meta name='og:title' content={siteTitle} />
+//                 <meta name='twitter:card' content='summary_large_image' />
+//                 <GoogleTags />
+//             </Head>
+//             <header className={styles.header}>
+//                 {home ? (
+//                     <>
+//                         <ProtoLogo />
+//                         <h1 className={utilStyles.heading2Xl}>{name}</h1>
+//                     </>
+//                 ) : (
+//                     <>
+//                         <Link href='/'>
+//                             <a>
+//                                 <Image
+//                                     priority
+//                                     src='/images/profile.jpg'
+//                                     className={utilStyles.borderCircle}
+//                                     height={108}
+//                                     width={108}
+//                                     alt={name}
+//                                 />
+//                             </a>
+//                         </Link>
+//                         <h2 className={utilStyles.headingLg}>
+//                             <Link href='/'>
+//                                 <a className={utilStyles.colorInherit}>{name}</a>
+//                             </Link>
+//                         </h2>
+//                     </>
+//                 )}
+//             </header>
+//             <menu>
+//                 <div className=''></div>
+//             </menu>
+//             <main>{children}</main>
+//             {!home && (
+//                 <div className={styles.backToHome}>
+//                     <Link href='/'>
+//                         <a>← Back to home</a>
+//                     </Link>
+//                 </div>
+//             )}
+//         </div>
+//     )
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,103 @@ import {getSortedPostsData} from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
+const About = () => {
+    return (
+        <div className='flex flex-col-reverse sm:flex-row items-start'>
+            <div className='flex flex-col pr-8'>
+                <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white'>
+                    Raman Ramanouski
+                </h1>
+                <h2 className='text-gray-700 dark:text-gray-200 mb-4'>
+                    Solution Architects at <span className='font-semibold'>EIS</span>
+                </h2>
+                <p className='text-gray-600 dark:text-gray-400 mb-16'>
+                    Passionate about new technologies and long talks about the approach to programming.
+                </p>
+            </div>
+            <div className='w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto'>
+                <span
+                    style={{
+                        boxSizing: 'border-box',
+                        display: 'inline-block',
+                        overflow: 'hidden',
+                        width: 'initial',
+                        height: 'initial',
+                        background: 'none',
+                        opacity: 1,
+                        border: 0,
+                        margin: 0,
+                        padding: 0,
+                        position: 'relative',
+                        maxWidth: '100%'
+                    }}
+                >
+                    <span
+                        style={{
+                            boxSizing: 'border-box',
+                            display: 'block',
+                            width: 'initial',
+                            height: 'initial',
+                            background: 'none',
+                            opacity: 1,
+                            border: 0,
+                            margin: 0,
+                            padding: 0,
+                            maxWidth: '100%'
+                        }}
+                    >
+                        <img
+                            alt='Raman Ramanouski'
+                            aria-hidden='true'
+                            src='data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTc2IiBoZWlnaHQ9IjE3NiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
+                            style={{
+                                display: 'block',
+                                maxWidth: '100%',
+                                width: 'initial',
+                                height: 'initial',
+                                background: 'none',
+                                opacity: 1,
+                                border: 0,
+                                margin: 0,
+                                padding: 0
+                            }}
+                        />
+                    </span>
+                    <div
+                        className='rounded-full filter grayscale'
+                        data-nimg='intrinsic'
+                        style={{
+                            position: 'absolute',
+                            inset: 0,
+                            boxSizing: 'border-box',
+                            padding: 0,
+                            border: 'none',
+                            margin: 'auto',
+                            display: 'block',
+                            width: 0,
+                            height: 0,
+                            minWidth: '100%',
+                            maxWidth: '100%',
+                            minHeight: '100%',
+                            maxHeight: '100%',
+                            background: 'black'
+                        }}
+                    />
+                </span>
+            </div>
+        </div>
+    )
+}
+
 export default function Home({allPostsData}) {
     return (
-        <Layout home>
+        <Layout>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <section className={utilStyles.headingMd}>
-                <a href="https://status.ra2.dev/">Status Page </a>
-            </section>
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
+            <section>
+                <About />
+                <ul>
                     {allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href={`/posts/${id}`}>
