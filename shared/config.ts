e265b0bf6caa20a9d7fbcb58a117eb88aa08e097
rramanouski <rramanouski@eisgroup.com>
2022-01-22 23:50:50 +0100

    Small update.

---
+++ shared/config.ts
@@ -0,0 +1,30 @@
+export const navLinks = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'About',
+        path: '/about'
+    },
+    {
+        title: 'Newsletter',
+        path: '/newsletter'
+    },
+    {
+        title: 'Uses',
+        path: '/users'
+    },
+    {
+        title: 'Guestbook',
+        path: '/guestbook'
+    },
+    {
+        title: 'Snippets',
+        path: '/snippets'
+    },
+    {
+        title: 'Tweets',
+        path: '/tweets'
+    }
+]
